ce27fe3357dd480137fce29cd2c235946fba5b3c
Anat0leg <anatolijrodionov345@gmail.com>
2023-04-08 02:05:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function Menu_show()
+{
+    document.getElementById("menu").style.display = "flex";
+    document.getElementById("menu-strips").style.display = "none";
+}
+
+function Menu_hide()
+{
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("menu-strips").style.display = "flex";
+}
