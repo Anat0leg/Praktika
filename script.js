c1681c8506d5366c6e3a59a60ec42d157abbcb98
Anat0leg <129735467+Anat0leg@users.noreply.github.com>
2023-04-20 05:21:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
+
 function Menu_show()
 {
-    //document.getElementById("menu").style.display = "flex";
+    
     document.getElementById("strips_box").style.display = "none";
     document.getElementById("CoNtEnT").style.justifyContent = "center";
 
     document.getElementById("menu").style.display ="block";
 }
 
-/*
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-
-*/
 function Menu_hide()
 {
-    //document.getElementById("menu").style.display = "none";
+    
     document.getElementById("strips_box").style.display = "flex";
     document.getElementById("CoNtEnT").style.justifyContent = "space-between";
 
